91db1fc34c136b3a31a5694ddee5207876ebdfc1
charles <goldraong57@gmail.com>
2024-12-20 05:00:20 +0000

    update config file

--- config/__init__.py
+++ config/__init__.py
@@ -1,18 +1,18 @@
 import os
 from dotenv import load_dotenv
 
-import configparser
-
 # Load the environment variables from the .env file
 load_dotenv()
 
 # Load the configuration file
-config = configparser.ConfigParser()
-config.read(filenames=os.path.join('config', os.getenv(key='MODE') + '.conf'))
 
-ORCHESTRATION_ADDRESS = config.get(section='orchestration', option='address')
-MONGODB_ADDRESS = config.get(section='mongodb', option='address')
+# orchestration
+ORCHESTRATION_ADDRESS = os.getenv(key="ORCHESTRATION_ADDRESS")
 
-MINIO_ADDRESS = config.get(section='minio', option='address')
-MINIO_ACCESS_KEY = os.getenv(key='MINIO_ACCESS_KEY')
-MINIO_SECRET_KEY = os.getenv(key='MINIO_SECRET_KEY')
+# mongodb
+MONGODB_ADDRESS = os.getenv(key="MODE")
+
+# minio
+MINIO_ADDRESS = os.getenv(key="MINIO_ADDRESS")
+MINIO_ACCESS_KEY = os.getenv(key="MINIO_ACCESS_KEY")
+MINIO_SECRET_KEY = os.getenv(key="MINIO_SECRET_KEY")
